3ff0e23c772685b19568abef62aeec3b585c0730
Jzavala96 <joshua962009@hotmail.com>
2025-02-24 08:43:56 -0500

    firebase fix?

--- firebase-config.js
+++ firebase-config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCRuRHDJ0GC6vtnSCGAcQCtE9yNaLJ1M_Y",
     authDomain: "glucoguardian-b935a.firebaseapp.com",
     projectId: "glucoguardian-b935a",
-    storageBucket: "glucoguardian-b935a.firebasestorage.app",
+    storageBucket: "glucoguardian-b935a.appspot.com",
     messagingSenderId: "79327666113",
     appId: "1:79327666113:web:f0cf092399dd1b6dbd9940"
   };
